f8bfb1f04be66a84ef6fd77037e7f5d67363ec03
dalton driver <jared.ddriver@gmail.com>
2024-11-19 11:59:27 -0600

    updates

---
+++ EmployeeSQL/table_schemata.sql
@@ -0,0 +1,43 @@
+CREATE TABLE titles (
+    title_id VARCHAR PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR,
+    birth_date DATE,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex VARCHAR,
+    hire_date DATE,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries (
+    emp_no INT,
+    salary INT NOT NULL,
+    PRIMARY KEY (emp_no, salary),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR,
+    emp_no INT,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
